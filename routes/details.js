df4c112436e4394bf89f67d5c5160bf3e5f055e9
Tomas Dvorak <todvora@gmail.com>
2015-05-15 14:14:00 +0200

    purecss fw, design, readme of plugins

--- routes/details.js
+++ routes/details.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var plugins = require('variety-plugins-searcher')
 var router = express.Router();
+var marked = require('marked');
 
 router.get('/*', function(req, res, next) {
   var pluginName = req.url.substring(1);
   plugins.getDetails(pluginName)
   .then(function(data){
-    res.render('details', { plugin: data});
+    res.render('details', {plugin: data, md: marked});
   })
   .fail(function(err){
     res.render('error', { 'message':'Failed to load plugin details' , error: err});
